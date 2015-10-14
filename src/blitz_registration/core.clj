041d903ecc955112ec345f23c599438814b2645c
Alexandre Gariépy <gariepy.alex@gmail.com>
2015-10-13 22:17:52 -0400

    Correction regex Un cas où, par exemple, le paragraphe contient le mot appartement devra être considéré comme contenant le mot ment aussi.

--- src/blitz_registration/core.clj
+++ src/blitz_registration/core.clj
@@ -56,7 +56,7 @@
 
 (defn indices-of-paragraphs-matching-word
   [word paragraphs]
-  (let [word-regex (re-pattern (str "(?i).*(?:\\s|^)" word "(?:\\s|$).*"))]
+  (let [word-regex (re-pattern (str "(?i).*" word ".*"))]
     (into [] (map first
                   (filter #(re-matches word-regex (second %)) paragraphs)))))
 
